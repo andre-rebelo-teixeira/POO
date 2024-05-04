626bb8fe6e52a72bcb9ddb1b6115f03b21f14068
andre-rebelo-teixeira <andre.r.teixeira@tecnico.ulisboa.pt>
2024-05-04 21:01:08 +0000

    feat(Exp Dist): simple class and interfave for a distribution funtion

---
+++ ExponentialDistribution/ExponentialDistribution.java
@@ -0,0 +1,120 @@
+package ExponentialDistribution
+
+import java.util.Random;   
+
+/*
+    * ExponentialDistribution.java
+    * Simple class to model an exponential distribution
+    * 
+    * Created on 16/06/2023
+*/
+public class ExponentialDistribution implements ExponentialDistributionInterface{
+    private float lambda = 0.0f;
+
+    /**
+     * Constructor for the ExponentialDistribution class
+     * 
+     * @param lambda The rate parameter of the exponential distribution
+     */
+    public ExponentialDistribution(float lambda){
+        this.lambda = lambda;
+    }
+
+    /*
+        * Get the lambda of the distribution 
+        *
+        * @return The lambda of the distribution
+        */
+    public float getLambda(){
+        return lambda;
+    }
+
+    /*
+        * Set the lambda of the distribution
+        *
+        * @param lambda The lambda of the distribution
+        */
+    public void setLambda(float lambda){
+        this.lambda = lambda;
+    }
+
+    /*
+        * Get the mean of the distribution
+        *
+        * @return The mean of the distribution
+        */
+    public float getMean(){
+        return 1/lambda;
+    }
+
+    /*
+        * Set the mean of the distribution
+        *
+        * @param mean The mean of the distribution
+        */
+    public void setMean(float mean){
+        this.lambda = 1/mean;
+    }
+
+    /*
+        * Get the variance of the distribution
+        *
+        * @return The variance of the distribution
+        */
+    public float getVariance(){
+        return 1/(lambda*lambda);
+    }
+
+    /*
+        * Set the variance of the distribution
+        *
+        * @param variance The variance of the distribution
+        */
+    public float getPDF(float x){
+        return lambda*(float)Math.exp(-lambda*x);
+    }
+
+    /* 
+        * Get the cumulative distribution function of the distribution
+        *
+        * @param x The value to evaluate the CDF at
+        * @return The value of the CDF at x
+        */
+    public float getCDF(float x){
+        return 1-(float)Math.exp(-lambda*x);
+    }
+
+    /*
+        * Get the quantile of the distribution
+        *
+        * @param p The probability to get the quantile for
+        * @return The quantile for the given probability
+        */
+    public float getQuantile(float p){
+        return (float)Math.log(1-p)/(-lambda);
+    }
+
+    /*
+        * Get a random value from the distribution
+        *
+        * @return A random value from the distribution
+        */
+    public float getExponentialVal(float x) {
+        if (x < 0) {
+            return 0;
+        }
+
+        return (float) this.lambda * Math.exp(-this.lambda • x);
+    }
+
+    /*
+        * Get a random value from the distribution
+        *
+        * @return A random value from the distribution
+        */
+    public float getExponentialRandom() {
+        Random rand = new Random();
+
+        return this.getExponentialVal(rand.nextFloat());
+    }
+}
---
+++ ExponentialDistribution/ExponentialDistributionInterface.java
@@ -0,0 +1,86 @@
+package ExponentialDistribution;
+
+
+/**
+ * ExponentialDistributionInterface.java
+ * Interface for the ExponentialDistribution class
+ * 
+ * Created on 16/06/2023
+ */
+public inteface ExponentialDistributionInterface {
+    
+    /*
+        * Get the lambda of the distribution 
+        *
+        * @return The lambda of the distribution
+        */
+    public float getLambda();
+    
+    /*
+        * Set the lambda of the distribution
+        *
+        * @param lambda The lambda of the distribution
+        */
+    public void setLambda(float lambda);
+    
+    /*
+        * Get the mean of the distribution
+        *
+        * @return The mean of the distribution
+        */
+    public float getMean();
+
+    /*
+        * Set the mean of the distribution
+        *
+        * @param mean The mean of the distribution
+        */ 
+    public void setMean(float mean);
+
+    /*
+        * Get the variance of the distribution
+        *
+        * @return The variance of the distribution
+        */
+
+    public float getVariance();
+
+    /*
+        * Get the probability density function of the distribution
+        *
+        * @param x The value at which the pdf is to be calculated
+        * @return The pdf of the distribution at x
+        */    
+    public float getPdf(flaot x);
+    
+    /*
+        * Get the cumulative distribution function of the distribution
+        *
+        * @param x The value at which the cdf is to be calculated
+        * @return The cdf of the distribution at x
+        */
+    public float getCdf(float x);
+
+    /*
+        * Get the quantile of the distribution
+        *
+        * @param p The probability at which the quantile is to be calculated
+        * @return The quantile of the distribution at p
+        */ 
+    public float getQuantile(float p);
+    
+    /*
+        * Get the exponential value of the distribution
+        *
+        * @param x The value at which the exponential value is to be calculated
+        * @return The exponential value of the distribution at x
+        */
+    public float getExponentialVal(float x);
+
+    /*
+        * Get the exponential random value of the distribution
+        *
+        * @return The exponential random value of the distribution
+        */
+    public float getExponentialRandom();
+}
