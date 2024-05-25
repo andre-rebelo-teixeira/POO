bbecd7784230252c2d6fb9596edd8c453273e2ab
andre-rebelo-teixeira <andre.r.teixeira@tecnico.ulisboa.pt>
2024-05-25 11:41:14 +0100

    feat(Main): Create a simple main package to test stuff

--- Main/Main.java
+++ Main/Main.java
@@ -1,4 +1,19 @@
 package Main;
 
+import population.Population;
+
 public class Main {
+    public static void main (String[] args) {
+        System.out.println("Imperial Safety Simulation starting with the given parameters");
+
+        int cost[][] = {    {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 14, 15, 16, 17, 18, 18, 19, 20},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 14, 15, 16, 17, 18, 18, 19, 20},
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12, 14, 15, 16, 17, 18, 18, 19, 20}};
+
+        Population pop = new Population(4, 20, 3, cost);
+
+
+
+
+    }
 }
